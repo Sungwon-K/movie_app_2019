0e443f2de4734f0153f0931865c86bc31f033c57
DESKTOP-CJQ33DQ\etrees <lunell513@naver.com>
2021-03-07 22:05:05 +0900

    2021.03

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Food({ name,picture }){
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture}/>
+    </div>
+  )
+}
+
+const foodILike = [
+  {
+      name : "kimchi",
+      image:
+        "http//aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+  },
+  {
+    name : "Samgyeopsal",
+    image:
+      "http//3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAAfLk/yHxnxFXcfx4ZKSfHS_RQNKjw3bA.jpg"
+  },
+  {
+    name : "bibimbap",
+    image:
+      "http//cdn-image/myrecipes.com/sites/default/files/styles/4_3_horizonatal_-_1200x900/public.jpg"
+  },
+  {
+    name : "Doncasu",
+    image:
+      "http//s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRyAu5feA/ls.jpg"
+  },
+  {
+    name : "Kimbap",
+    image:
+      "http//cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+  }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
+      {foodILike.map(dish => <Food name={dish.name} picture ={dish.image}/>)}
     </div>
-  );
+  )
+
 }
 
 export default App;
